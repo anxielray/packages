4be39db6c73dca678deae020a7b50fcdd40370e7
anxielray <anxielworld@gmail.com>
2024-12-21 13:05:20 +0300

    [Add]: Add the functions to handle squaees and square roots of numbers as integrers. [Update]: Update the name of the first function to Exponenets to avoid confusion with the new function

--- IntPow/main.go
+++ IntPow/main.go
@@ -2,6 +2,14 @@ package IntPow
 
 import "math"
 
-func Sqr(x, y int) int {
+func Exponential(x, y int) int {
 	return int(math.Pow(float64(x), float64(y)))
+}
+
+func Sqr(x int) int {
+	return x * x
+}
+
+func Sqrt(x int) int {
+	return int(math.Sqrt(float64(x)))
 }
